8d352c8eedcff9ecf2247766a0bca16ebc8d5de6
LedyaBakloug <ledya.beshara@yahoo.com>
2024-01-04 12:16:59 -0500

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { EducationComponent } from './components/education/education.component';
+import { WorkHistoryComponent } from './components/work-history/work-history.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', 'title': 'Home', component: HomeComponent },
+    { path: 'contact', 'title': 'Contact', component: ContactComponent },
+    { path: 'education', 'title': 'Education', component: EducationComponent },
+    { path: 'skills', 'title': 'Skills', component: SkillsComponent },
+    { path: 'work-history', 'title': 'Work History', component: WorkHistoryComponent },
+    { path: '**', component: PageNotFoundComponent}
+
+];
+
